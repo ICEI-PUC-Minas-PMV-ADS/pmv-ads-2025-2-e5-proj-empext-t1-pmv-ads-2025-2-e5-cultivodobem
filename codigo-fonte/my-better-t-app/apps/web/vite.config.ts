827cfddf0a152339542c8944eefca775370168a6
juan-20 <juanalvesandrade@gmail.com>
2025-09-15 19:09:59 -0300

    creating project

---
+++ codigo-fonte/my-better-t-app/apps/web/vite.config.ts
@@ -0,0 +1,25 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [tailwindcss(), tanstackRouter({}), react(), VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "my-cultivoapp",
+        short_name: "my-cultivoapp",
+        description: "my-cultivoapp - PWA Application",
+        theme_color: "#0c0c0c",
+      },
+      pwaAssets: { disabled: false, config: true },
+      devOptions: { enabled: true },
+    })],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
